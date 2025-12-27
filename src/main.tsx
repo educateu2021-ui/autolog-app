9077c8f903e6e63e822fed3e371c983be9d1a286
educateu2021-ui <educateu2021@gmail.com>
2025-12-27 18:39:45 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import AutologApp from './App'
+import AutologApp from './AutologApp.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AutologApp />
-  </React.StrictMode>
+  </React.StrictMode>,
 )
